f34b058d5c1b968a3697c67e0ba743a51aa1b831
vicaba <victor.caballero.p@outlook.com>
2023-08-20 00:05:31 +0200

    feat(test): add testing dependency MUnit

--- src/test/scala/phr/hippo/api/http/record/infrastructure/RecordRoutesSpec.scala
+++ src/test/scala/phr/hippo/api/http/record/infrastructure/RecordRoutesSpec.scala
@@ -8,20 +8,5 @@ import org.http4s.client.dsl.Http4sClientDsl
 import org.http4s.dsl.Http4sDsl
 import org.http4s.syntax.kleisli.*
 import org.http4s.{ EntityDecoder, EntityEncoder, Uri }
-import org.scalatest.{ FunSuite, Matchers }
-import org.scalatestplus.scalacheck.ScalaCheckPropertyChecks
 
-class RecordRoutesSpec
-    extends FunSuite
-       with Matchers
-       with ScalaCheckPropertyChecks
-       with PetStoreArbitraries
-       with Http4sDsl[IO]
-       with Http4sClientDsl[IO]:
-  "A Stack" should "pop values in last-in-first-out order" in {
-    val stack = new Stack[Int]
-    stack.push(1)
-    stack.push(2)
-    stack.pop() should be(2)
-    stack.pop() should be(1)
-  }
+// https://www.youtube.com/watch?v=YO_hIUAxS60&t=4s
