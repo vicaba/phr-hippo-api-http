e3dc228d40617c4dd329dd3c5fbddd8a9e08bf5a
vicaba <victor.caballero.p@outlook.com>
2023-08-26 19:08:44 +0200

    test: add Create Record test

--- src/test/scala/phr/hippo/api/http/record/infrastructure/RecordRoutesSuite.scala
+++ src/test/scala/phr/hippo/api/http/record/infrastructure/RecordRoutesSuite.scala
@@ -1,26 +1,22 @@
 package phr.hippo.api.http.record.infrastructure
 
 import java.util.UUID
-
 import cats.Applicative
-import cats.effect.*
+import cats.effect.IO
 import cats.syntax.all.*
-
 import io.circe.*
 import io.circe.syntax.*
-
 import io.circe.parser.*
 import munit.*
-
 import org.http4s.*
-
 import org.http4s.dsl.io.*
 import org.http4s.dsl.*
 import org.http4s.implicits.*
-
 import phr.hippo.api.http.record.application.RecordService
 import phr.hippo.api.http.record.domain.*
 import phr.hippo.api.http.record.infrastructure.formats.RecordJsonFormat.given
+
+import scala.util.Try
 
 class RecordRoutesSuite extends munit.Http4sHttpRoutesSuite:
   val dummyRecord: Record = Record.dummy
@@ -35,9 +31,16 @@ class RecordRoutesSuite extends munit.Http4sHttpRoutesSuite:
   override val routes: HttpRoutes[IO] =
     RecordRoutes[IO](RecordService(recordRepository)).allEndpoints
 
-  test(GET(uri"record" / dummyRecord.header.id)).alias("Create Record") { response =>
+  test(GET(uri"record" / dummyRecord.header.id)).alias("Retrieve Record") { response =>
     val result = response.as[String].map(parse)
     val expected = dummyRecord.asJson
+
+    assertIO(result, Right(expected))
+  }
+
+  test(POST(uri"record").withEntity[IO, Record](dummyRecord)).alias("Create Record") { response =>
+    val result = response.as[String].map(n => Try(n.toInt).toEither)
+    val expected = 1
 
     assertIO(result, Right(expected))
   }
