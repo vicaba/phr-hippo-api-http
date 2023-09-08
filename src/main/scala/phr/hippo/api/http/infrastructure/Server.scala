8d864f73d1387d1d218bd41f3db0f37e7c75e1e0
vicaba <vicaba@me.com>
2023-09-08 13:24:28 +0200

    ci: change server port

--- src/main/scala/phr/hippo/api/http/infrastructure/Server.scala
+++ src/main/scala/phr/hippo/api/http/infrastructure/Server.scala
@@ -32,7 +32,7 @@ object Server extends IOApp:
       server <- EmberServerBuilder
         .default[F]
         .withHost(ipv4"0.0.0.0")
-        .withPort(port"8083")
+        .withPort(port"8084")
         .withHttpApp(allEndpoints.orNotFound)
         .build
     yield server
