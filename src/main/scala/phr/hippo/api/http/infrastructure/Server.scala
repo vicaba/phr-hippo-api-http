b06f2e4048ff645ffc118a0f51df84771fa8a40a
vicaba <vicaba@me.com>
2023-09-08 23:17:50 +0200

    ci: change ci file to test

--- src/main/scala/phr/hippo/api/http/infrastructure/Server.scala
+++ src/main/scala/phr/hippo/api/http/infrastructure/Server.scala
@@ -32,7 +32,7 @@ object Server extends IOApp:
       server <- EmberServerBuilder
         .default[F]
         .withHost(ipv4"0.0.0.0")
-        .withPort(port"8084")
+        .withPort(port"8081")
         .withHttpApp(allEndpoints.orNotFound)
         .build
     yield server
